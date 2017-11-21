575bc18bb114ed87e359ed1ecd9f05109048b460
Reid Paape <wrpaape@gmail.com>
2017-11-20 21:08:22 -0600

    plotting points

--- plscratch.c
+++ plscratch.c
@@ -1,57 +1,55 @@
-// Simple demo of a 2D line plot.
-//
-// Copyright (C) 2011  Alan W. Irwin
-//
-// This file is part of PLplot.
-//
-// PLplot is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Library General Public License as published
-// by the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// PLplot is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with PLplot; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-//
-//
-
 #include "plcdemos.h"
 #define NSIZE    101
 
+
+static const PLFLT xmin = 0.0;
+static const PLFLT xmax = 1.0;
+static const PLFLT ymin = 0.0;
+static const PLFLT ymax = 100.0;
+static const PLINT just = 0;
+static const PLINT axis = 0;
+
 int
-main( int argc, char *argv[] )
+main(int argc,
+     char *argv[])
 {
-    PLFLT x[NSIZE], y[NSIZE];
-    PLFLT xmin = 0., xmax = 1., ymin = 0., ymax = 100.;
-    int   i;
+	PLFLT x[NSIZE];
+	PLFLT y[NSIZE];
+	int   i;
 
-    // Prepare data to be plotted.
-    for ( i = 0; i < NSIZE; i++ )
-    {
-        x[i] = (PLFLT) ( i ) / (PLFLT) ( NSIZE - 1 );
-        y[i] = ymax * x[i] * x[i];
-    }
+	// Prepare data to be plotted.
+	for ( i = 0; i < NSIZE; i++ ) {
+		x[i] = (PLFLT) ( i ) / (PLFLT) ( NSIZE - 1 );
+		y[i] = ymax * x[i] * x[i];
+	}
 
-    // Parse and process command line arguments
-    plparseopts( &argc, argv, PL_PARSE_FULL );
+	// Parse and process command line arguments
+	plparseopts( &argc, argv, PL_PARSE_FULL );
 
-    // Initialize plplot
-    plinit();
+	// Initialize plplot
+	plinit();
 
-    // Create a labelled box to hold the plot.
-    plenv( xmin, xmax, ymin, ymax, 0, 0 );
-    pllab( "x", "y=100 x#u2#d", "Simple PLplot demo of a 2D line plot" );
+	// Create a labelled box to hold the plot.
+	plenv(xmin, xmax,
+	      ymin, ymax,
+	      just, axis);
 
-    // Plot the data that was prepared above.
-    plline( NSIZE, x, y );
+	pllab("x",
+	      "y=100 x#u2#d",
+	      "Simple PLplot demo of a 2D line plot");
 
-    // Close PLplot library
-    plend();
+	// Plot the data that was prepared above.
+	plline(NSIZE, x, y);
 
-    exit( 0 );
+	PLFLT point_x = (xmax - xmin) / 2.0;
+	PLFLT point_y = (ymax - ymin) / 2.0;
+
+	plstring(1,
+		 &point_x, &point_y,
+		 "o");
+
+	// Close PLplot library
+	plend();
+
+	return 0;
 }
